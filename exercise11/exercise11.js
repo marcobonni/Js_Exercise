c467707e15a8560b94508d7caadd140ce8cd8ea4
marcobonni <marco.bonni19@gmail.com>
2023-01-15 18:21:19 +0100

    first commit

---
+++ exercise11/exercise11.js
@@ -0,0 +1,18 @@
+function adultFilter(Array, age) {
+  return Array.filter((Element) => Element.age >= age);
+}
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+const adults = adultFilter(persons, 18);
+console.log(persons);
+console.log(adults);
