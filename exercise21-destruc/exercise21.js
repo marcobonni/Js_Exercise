2298dd79703931a292c5f109337af0fa65f32810
marcobonni <marco.bonni19@gmail.com>
2023-01-25 02:39:52 +0100

    first upload

---
+++ exercise21-destruc/exercise21.js
@@ -0,0 +1,9 @@
+let num1 = 10;
+let num2 = 20;
+console.log('Before swap: ', num1, num2); // Before swap: 10 20
+// Variable swapping
+let arr = [num1, num2];
+num1 = arr[1];
+num2 = arr[0];
+
+console.log('After swap: ', num1, num2); // After swap: 20 10
