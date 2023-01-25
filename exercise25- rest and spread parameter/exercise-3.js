e1c91c7ee2aa7b41a9329c242fb3eb18fcb0d4a6
marcobonni <marco.bonni19@gmail.com>
2023-01-25 04:31:46 +0100

    first try

---
+++ exercise25- rest and spread parameter/exercise-3.js
@@ -0,0 +1,9 @@
+// let newNumber = 3;
+// let numberStore = [0, 1, 2, newNumber];
+// console.log(numberStore);
+
+// alternative 
+let numberStoreplus = [0, 1, 2];
+let newNumberplus = 3;
+let merged = [...numberStoreplus, newNumberplus];
+console.log(merged);
