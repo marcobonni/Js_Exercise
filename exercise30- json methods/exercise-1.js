057292e1d9c0c84e7966e7e40c2e45fbe256a86a
marcobonni <marco.bonni19@gmail.com>
2023-01-30 09:29:30 +0100

    first try

---
+++ exercise30- json methods/exercise-1.js
@@ -0,0 +1,13 @@
+class Person {
+  constructor(id, firstName, lastName, age) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+}
+
+const developer = new Person(1, 'Mario', 'Rossi', 25);
+const JSONdeveloper = JSON.stringify(developer);
+// Print developer as json object
+console.log(JSONdeveloper);
